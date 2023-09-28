c0792567d885d5015b3bae3e04285ad6e1a47510
Endre Varga <vendere10@gmail.com>
2023-09-28 13:56:09 +0200

    lol

---
+++ src/main/java/com/example/loginappauth/LoginAppAuthApplication.java
@@ -0,0 +1,37 @@
+package com.example.loginappauth;
+
+import com.example.loginappauth.models.ApplicationUser;
+import com.example.loginappauth.models.Role;
+import com.example.loginappauth.repository.RoleRepository;
+import com.example.loginappauth.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@SpringBootApplication
+public class LoginAppAuthApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(LoginAppAuthApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner init(RoleRepository roleRepository, UserRepository userRepository, PasswordEncoder passwordEncoder) {
+		return args -> {
+			if (roleRepository.findByAuthority("ADMIN").isPresent()) return;
+			Role adminRole = roleRepository.save(new Role("ADMIN"));
+			roleRepository.save(new Role("USER"));
+
+			Set<Role> roles = new HashSet<>();
+			roles.add(adminRole);
+
+			ApplicationUser admin = new ApplicationUser(1, "admin", passwordEncoder.encode("password"), roles);
+
+			userRepository.save(admin);
+		};
+	}
+}
---
+++ src/test/java/com/example/loginappauth/LoginAppAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loginappauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginAppAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
